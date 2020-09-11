181c4bb9ed700ccba14ead463b1349010a3d5afb
Sergio Gómez <sergio@uco.es>
2020-09-11 22:37:15 +0200

    Deploy to gh-pages (#3)

--- components/Title/Title.tsx
+++ components/Title/Title.tsx
@@ -8,7 +8,7 @@ export const Title: React.FunctionComponent<Props> = ({ text }) => (
   <div className="flex m-1 p-1 items-center space-x-4">
     <img
       className="flex-shrink-0 h-5 w-5"
-      src="/assets/img/ellipse.svg"
+      src="assets/img/ellipse.svg"
       alt="logo"
     />
     <p className="text-white font-medium text-2xl">{text}</p>
