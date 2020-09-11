eb39a862468572e625d74408dca10e92707b6889
Sergio Gómez <sergio@uco.es>
2020-09-11 18:56:15 +0200

    Circular progress bar (#1)
    * Add CircularProgressBar component with story
    * Add react-konva
    * Add dark theme to storybook
    * Set tsconfig strict
    * Add basic test
    * Update dependencies

---
+++ components/CircularProgressBar/CircularProgressBar.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import React from 'react';
+
+import CircularProgressBar, { Props } from './CircularProgressBar';
+
+export default {
+  component: CircularProgressBar,
+  title: 'Widgets/CircularProgressBar',
+} as Meta;
+
+const Template: Story<Props> = (args) => <CircularProgressBar {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  range: 10,
+  value: 8,
+};
---
+++ components/CircularProgressBar/CircularProgressBar.tsx
@@ -0,0 +1,65 @@
+import useWindowSize from '@rehooks/window-size';
+import React from 'react';
+import { Arc, Circle, Layer, Stage, Text } from 'react-konva';
+
+export interface Props {
+  range: number;
+  value: number;
+  radius: number;
+}
+
+export const CircularProgressBar: React.FunctionComponent<Props> = ({
+  range,
+  value,
+  radius = 400,
+}) => {
+  const limit = range <= 0 ? 1 : range;
+  const counter = value < 0 ? 0 : value > limit ? limit : value;
+
+  const windowSize = useWindowSize();
+  const size = Math.min(windowSize.innerHeight, windowSize.innerWidth, radius);
+
+  const angle = (360 * counter) / limit;
+
+  return (
+    <div data-testid="circular-progress-bar">
+      <Stage width={size} height={size}>
+        <Layer>
+          <Circle
+            x={size / 2}
+            y={size / 2}
+            stroke="#1B143F"
+            radius={size * 0.4}
+            strokeWidth={size * 0.08}
+            shadowBlur={20}
+          />
+          <Arc
+            x={size / 2}
+            y={size / 2}
+            innerRadius={size * 0.4}
+            outerRadius={size * 0.4}
+            angle={angle}
+            lineCap="round"
+            lineJoin="round"
+            strokeWidth={size * 0.08}
+            stroke={'#7012CE'}
+            rotation={-90}
+          />
+          <Text
+            text={counter.toString()}
+            fill={'white'}
+            fontSize={size * 0.2}
+            fontStyle={'bold'}
+            fontFamily={'Lato'}
+            verticalAlign={'middle'}
+            align={'center'}
+            width={size}
+            height={size}
+          />
+        </Layer>
+      </Stage>
+    </div>
+  );
+};
+
+export default CircularProgressBar;
