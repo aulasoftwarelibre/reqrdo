5e228ec0c05cb534a5e9b27c543325fb0d3c4a19
Sergio Gómez <sergio@uco.es>
2020-09-12 11:10:40 +0200

    Add timer component

---
+++ components/Timer/Timer.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import React from 'react';
+
+import { Props, Timer } from './Timer';
+
+export default {
+  component: Timer,
+  title: 'Widgets/Timer',
+} as Meta;
+
+const Template: Story<Props> = (args) => <Timer {...args} />;
+
+const from = new Date();
+from.setTime(from.valueOf() - 1000 * 3600 * 6.5); // milliseconds * 6.5 hours
+
+const to = new Date();
+to.setTime(to.valueOf() - 1000 * 3600 * 3.2); // milliseconds * 3.2 hours
+
+export const FromDateToNow = Template.bind({});
+FromDateToNow.args = {
+  from: from,
+};
+
+export const BetweenTwoDates = Template.bind({});
+BetweenTwoDates.args = {
+  from: from,
+  to: to,
+};
