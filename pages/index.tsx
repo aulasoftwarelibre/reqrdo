40cb88faf5997ac00c8b4e6c4fbfbf204fbd83a2
Sergio Gómez <sergio@uco.es>
2020-09-14 01:21:12 +0200

    Read status from API in front page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,37 @@
 import { NextPage } from 'next';
+import React from 'react';
+import useFetch from 'use-http';
 
 import { CircularProgressBar, Title } from '../components';
 
+interface Status {
+  capacity: number;
+  occupation: number;
+}
+
 const Home: NextPage = () => {
+  const [status, setStatus] = React.useState<Status>();
+  const { get, response } = useFetch('/api');
+
+  React.useEffect(() => {
+    init();
+  }, []);
+
+  async function init() {
+    const initStatus: Status = await get('/status');
+    if (response.ok) setStatus(initStatus);
+  }
+
   return (
     <div className="flex min-h-full">
       <div className="m-auto space-y-5">
         <Title text="¿Cuánta gente hay en el aula?" />
-        {process.browser && (
-          <CircularProgressBar range={10} value={3} radius={400} />
+        {process.browser && status && (
+          <CircularProgressBar
+            range={status.capacity}
+            value={status.occupation}
+            radius={400}
+          />
         )}
       </div>
     </div>
