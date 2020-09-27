3d591a739bdab2376d9422d95bfdb82117f85cdd
Sergio Gómez <sergio@uco.es>
2020-09-27 10:11:00 +0200

    Add check-in and check-out actions

---
+++ api/src/Exception/CheckRequestException.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exception;
+
+use Exception;
+
+use function sprintf;
+
+class CheckRequestException extends Exception
+{
+    public static function becauseRoomDoesNotExists(string $id): self
+    {
+        return new self(sprintf('The room "%s" does not exists', $id));
+    }
+
+    public static function becauseOnlyOneOpenCheckIsAllowed(): self
+    {
+        return new self('Previous check must be closed');
+    }
+
+    public static function becauseNoOpenCheckIsFound(): self
+    {
+        return new self("No open check was found");
+    }
+}
