a354f41675ca1fd63dd70248c5ddeb3f24b40d59
Sergio Gómez <sergio@uco.es>
2020-09-28 23:45:36 +0200

    [client-web] Add notification list to layout

--- client-web/components/Layout/Layout.tsx
+++ client-web/components/Layout/Layout.tsx
@@ -2,12 +2,14 @@ import { useSession } from 'next-auth/client';
 import React from 'react';
 
 import { Navbar } from '../Navbar';
+import { NotificationList } from '../Notification';
 
 export const Layout: React.FunctionComponent = ({ children }) => {
   const [session] = useSession();
 
   return (
     <div className="min-h-screen flex flex-col bg-black text-white">
+      <NotificationList />
       <Navbar session={session} />
       <main className="flex flex-col flex-grow overflow-hidden">
         {children}
